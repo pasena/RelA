86ad3f0b00677ab0d146a411a589bb765ad1a3e5
Paulo Sena <Paulo Sena>
2012-07-08 00:49:07 -0300

    Authentication

---
+++ RelA.WebUI/Infrastructure/Abstract/IAuthenticationProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RelA.WebUI.Infrastructure.Abstract
+{
+    public interface IAuthenticationProvider
+    {
+        bool Authenticate(string userName, string password);
+    }
+}
---
+++ RelA.WebUI/Infrastructure/RelAAuthentication.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RelA.WebUI.Infrastructure.Abstract;
+using RelA.Domain.Abstract;
+using RelA.Domain.Entities;
+using System.Web.Mvc;
+
+namespace RelA.WebUI.Infrastructure
+{
+    public class RelAAuthentication : IAuthenticationProvider
+    {
+        private IUserRepository userRepository = null;
+        private string authenticationSessionKey = "UserSessionKey";
+
+        public RelAAuthentication(IUserRepository repo)
+        {
+            this.userRepository = repo;
+        }
+
+        bool IAuthenticationProvider.Authenticate(string userName, string password)
+        {
+            bool result = false;
+
+            User authenticateUser = this.userRepository.GetAll.FirstOrDefault(w => w.Login == userName && w.Password == password);
+
+            if (authenticateUser != null)
+            {
+                HttpContext.Current.Session.Add(authenticationSessionKey, authenticateUser);
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
